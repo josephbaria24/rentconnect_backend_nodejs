2b52298e07d1c4908027d22ac9e5c870bc3baf6e
Joseph <202180024@psu.palawan.edu.ph>
2024-10-11 02:43:37 +0800

    update

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -33,7 +33,7 @@ exports.login = async(req,res,next)=>{
         await user.save();
 
         let tokenData = {_id:user._id,email:user.email};
-        const token = await UserService.generateToken(tokenData,"secretKey",'1h');
+        const token = await UserService.generateToken(tokenData,"secretKey",'2d');
         res.status(200).json({status:true, token:token})
 
     } catch(error) {
