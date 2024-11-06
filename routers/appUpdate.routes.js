cafd75ed942e6c9804da6b0906dc03229ef40a30
Joseph <202180024@psu.palawan.edu.ph>
2024-11-07 04:04:22 +0800

    updater added

--- routers/appUpdate.routes.js
+++ routers/appUpdate.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Endpoint to return the latest version and APK URL
 router.get('/download/version', (req, res) => {
   const versionInfo = {
-    latest_version: "1.0.2", // Update this whenever you release a new version
+    latest_version: "1.0.1", // Update this whenever you release a new version
     download_url: "https://drive.google.com/uc?export=download&id=1P2tPlp8TpVQwkTBfBlHMjfpCulP4KRVP"
   };
   
